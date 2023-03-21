2327d617312dc0c291a60196e03e21be37149307
Max Alcala <maxarturo@gmail.com>
2023-03-21 18:33:10 -0300

    feat: extension on riverpod for Either

--- lib/src/adapters/location/location.dart
+++ lib/src/adapters/location/location.dart
@@ -19,7 +19,7 @@ class LocationAdapter {
       if (!_serviceEnabled) {
         _serviceEnabled = await _systemLocation.requestService();
         if (!_serviceEnabled) {
-          throw ServiceUnavailableError();
+          throw LocationServiceError();
         }
       }
 
