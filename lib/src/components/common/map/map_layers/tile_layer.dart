d3755b0b5c13b14fb5ad5e413b85cc28c23223fd
Max Alcala <maxarturo@gmail.com>
2023-04-13 10:46:52 -0300

    feat: update OSM API url for proxy

--- lib/src/components/common/map/map_layers/tile_layer.dart
+++ lib/src/components/common/map/map_layers/tile_layer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vamonos_mgp/src/adapters/map_tile/map_tile_provider.dart';
@@ -10,7 +11,7 @@ class TileLayerWidget extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return TileLayer(
       tileProvider: ref.watch(cachedTileAdapterProvider),
-      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      urlTemplate: "${dotenv.env['OSM_API_URL']!}/{z}/{x}/{y}.png",
       userAgentPackageName: 'com.ar.vamonosmpg.app',
     );
   }
