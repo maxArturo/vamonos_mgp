9ba850746b126879e48365931e8c50c06035fa25
Max Alcala <maxarturo@gmail.com>
2023-04-17 14:20:19 -0300

    fix: upstream headers

--- lib/src/services/mgp_route/config.dart
+++ lib/src/services/mgp_route/config.dart
@@ -1,10 +1,11 @@
-import 'package:dio/dio.dart';
+import 'dart:io';
 
 final baseApiHeaders = {
-  "Accept": "application/json, text/javascript, */*; q=0.01",
-  "Accept-Encoding": "gzip, deflate",
-  "Accept-Language": "en-US,en;q=0.5",
-  "Content-Type": Headers.formUrlEncodedContentType,
+  HttpHeaders.acceptHeader: "application/json, text/javascript, */*; q=0.01",
+  HttpHeaders.acceptEncodingHeader: "gzip, deflate",
+  HttpHeaders.acceptLanguageHeader: "en-US,en;q=0.5",
+  HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded',
   // for some reason the API looks for this specifically
-  'Referer': 'https://appsl.mardelplata.gob.ar/app_cuando_llega/cuando.php',
+  HttpHeaders.refererHeader:
+      'https://appsl.mardelplata.gob.ar/app_cuando_llega/cuando.php',
 };
