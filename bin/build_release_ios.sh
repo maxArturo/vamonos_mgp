d7e3039ff19f9f2a9b448f1178954b5081665191
Max Alcala <maxarturo@gmail.com>
2023-04-20 19:01:19 -0300

    chore: update build bin file

--- bin/build_release_ios.sh
+++ bin/build_release_ios.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
 
-flutter clean \
-        && rm ios/Podfile.lock pubspec.lock \
-        && rm -rf ios/Pods ios/Runner.xcworkspace \
-        && flutter build ipa --build-name=1.0.0 --build-number=4 \
-        --obfuscate --split-debug-info=1.0.0+4
+REQUIRED_VARS=("SEMVER" "BUILD_NUMBER")
+
+# Loop through required environment variables and check if they are set
+for VAR_NAME in "${REQUIRED_VARS[@]}"; do
+        if [[ -z "${!VAR_NAME}" ]]; then
+                echo "ERROR: Required environment variable $VAR_NAME is not set."
+                exit 1
+        fi
+done
+
+flutter clean &&
+        rm ios/Podfile.lock pubspec.lock &&
+        rm -rf ios/Pods ios/Runner.xcworkspace &&
+        flutter build ipa --build-name="$SEMVER" --build-number="$BUILD_NUMBER" \
+                --obfuscate --split-debug-info=build/versions/"$SEMVER"+"$BUILD_NUMBER"
