858259a9c47b2a0b2ccb3c92096aec654f9fe274
Max Arturo AS <maxArturo@users.noreply.github.com>
2023-05-14 11:31:13 -0700

    feat: [#40] automated beta releases for both ios and android (#43)
    * feat: provisional setup while we verify google play
    
    * fix: update supply and fastlane metadata
    
    * chore: reset version for release-please
    
    * fix: working beta deploys on both lanes

---
+++ bin/release_android_beta.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+SEMVER=$(cat <pubspec.yaml | awk '/version:/ {print $2}' | awk -F \+ '{print $1}')
+BUILD_NUMBER=$(cat <pubspec.yaml | awk '/version:/ {print $2}' | awk -F \+ '{print $2}')
+
+echo "Building release version of app for beta release in Google Play Store"
+echo "Semver is: $SEMVER"
+echo "build no is: $BUILD_NUMBER"
+
+flutter build appbundle \
+  --dart-define-from-file './env.prod.json' \
+  --build-name="$SEMVER" --build-number="$BUILD_NUMBER" \
+  --obfuscate --split-debug-info=build/android/versions/"$SEMVER"+"$BUILD_NUMBER"
+
+cd android
+fastlane supply --aab ../build/app/outputs/bundle/release/app-release.aab --track beta
