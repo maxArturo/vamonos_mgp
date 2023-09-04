6e9a42177a99c73b0ce4234c79e4a51f9dcf5f40
Max Alcala <maxarturo@gmail.com>
2023-09-04 16:02:05 -0300

    chore: bump deps

--- lib/src/components/home/routes_near_you_list/directed_routes_page/directed_routes_card/widget.dart
+++ lib/src/components/home/routes_near_you_list/directed_routes_page/directed_routes_card/widget.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vamonos_mgp/src/components/common/map/markers/marker.dart';
 import 'package:vamonos_mgp/src/components/home/routes_near_you_list/directed_routes_page/directed_routes_card/view.dart';
 
-class DirectedRouteCard extends ConsumerStatefulWidget {
+class DirectedRouteCard extends StatefulWidget {
   final StopMarker stop;
 
   const DirectedRouteCard({required this.stop, super.key});
 
   @override
-  ConsumerState<DirectedRouteCard> createState() =>
-      DirectedRouteCardController();
+  State<DirectedRouteCard> createState() => DirectedRouteCardController();
 }
 
-class DirectedRouteCardController extends ConsumerState<DirectedRouteCard> {
+class DirectedRouteCardController extends State<DirectedRouteCard> {
   @override
   Widget build(BuildContext context) {
     return DirectedRouteCardView(this);
